c09875a3fba99bbc117cda397efd221dc61d1762
Eric Helstad <erichelstad@gmail.com>
2022-10-10 21:22:51 -0500

    Add mouseover feature to turn squares green

--- js/eas.js
+++ js/eas.js
@@ -1,7 +1,10 @@
 const container = document.querySelector('#container');
 
+//Create 16x16 grid
 for (i = 0 ; i < 256 ; i++) {
     const square = document.createElement('div');
     square.classList.add('gridSquare');
     container.appendChild(square);
+
+    square.onmouseover = () => square.classList.add('hoverSquare');
 }
