e3fa930a450541bcc96dccb8b76109d76f37f0ca
Eric Helstad <erichelstad@gmail.com>
2022-10-16 19:13:17 -0500

    Add feature to change size of grid

--- js/eas.js
+++ js/eas.js
@@ -1,10 +1,35 @@
-const container = document.querySelector('#container');
-
-//Create 16x16 grid
-for (i = 0 ; i < 256 ; i++) {
-    const square = document.createElement('div');
-    square.classList.add('gridSquare');
-    container.appendChild(square);
-
-    square.onmouseover = () => square.classList.add('hoverSquare');
+//Function to create grid
+function createGrid(size) {
+    for (i = 0 ; i < (size * size) ; i++) {
+        const square = document.createElement('div');
+        square.classList.add('gridSquare');
+        container.appendChild(square);
+    
+        square.onmouseover = () => square.classList.add('hoverSquare');
+    }
 }
+//Function to remove all existing grid squares
+function resetGrid(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+    }    
+}
+
+//Function to get new size of grid and recreate the grid
+function reSize() {
+    let size = prompt("Enter a size less than 100 squares wide");
+    while (isNaN(size) || size > 100) {
+        size = prompt("Enter a size less than 100 squares wide");
+    }
+    resetGrid(container);
+    container.style.cssText = `grid-template-columns: repeat(${size}, 1fr)`;
+    createGrid(size);
+}
+
+const container = document.querySelector('#container');
+let size = 16;
+createGrid(size);
+
+const changeButton = document.querySelector('#change-button');
+changeButton.onclick = () => reSize();
+
